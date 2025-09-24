8fa086591cbfaaa9da6d989de0caa5b61c54c9e1
proyectobse2025-a11y <proyectobse2025@gmail.com>
2025-09-24 15:52:01 -0200

    Add files via upload

---
+++ GitHug Repository.sh
@@ -0,0 +1,7 @@
+# En tu computadora, en la carpeta de tu proyecto:
+git init
+git add .
+git commit -m "Mi primer sitio web"
+git branch -M main
+git remote add origin https://github.com/tu-usuario/mi-sitio-web.git
+git push -u origin main
